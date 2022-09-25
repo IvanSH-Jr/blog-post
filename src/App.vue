eb5f821869fc772f100fcc6d16442c04aff32880
IvanSH-Jr <ivan_parabel@mail.ru>
2022-09-25 19:52:42 +0300

    computed and watch expected

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <template>
   <div class="app__logo"><img alt="Vue logo" src="./assets/logo.png"></div>
   <h1>Страница с постами</h1>
-  <my-btn
-    @click="openCreatePost"
-  >Создание постов</my-btn>
+  <div class="app__btns">
+    <my-btn
+      @click="openCreatePost"
+    >
+      Создание постов
+    </my-btn>
+    <my-select
+      v-model="selectedSort"
+      :options = "sortOptions"
+    ></my-select>
+  </div>
+
   <my-create-post v-model:openWindow="createPostVisible">
     <post-form
       @create="createPost"
@@ -11,9 +20,11 @@
   </my-create-post>
 
   <post-list 
+    v-if="!postLoading"
     :posts="posts"
     @removePost="removePost"
   />
+  <div v-else class="loading">Идет загрузка...</div>
 </template>
 
 <script>
@@ -21,7 +32,7 @@ import PostForm from '@/components/PostForm.vue';
 import PostList from '@/components/PostList.vue';
 import MyCreatePost from './components/myUI/MyCreatePost.vue';
 import MyBtn from './components/myUI/MyBtn.vue';
-
+import axios from 'axios'
 export default {
   components:{
     PostForm,
@@ -32,14 +43,14 @@ export default {
   },
   data(){
     return{
-      posts:[
-        {id:1, title: 'JavaScript', body:'Описание поста'},
-        {id:2, title: 'JavaScript', body:'Описание поста'},
-        {id:3, title: 'JavaScript', body:'Описание поста'},
-        {id:4, title: 'JavaScript', body:'Описание поста'},
-        {id:5, title: 'JavaScript', body:'Описание поста'},
-      ],
-      createPostVisible: false
+      posts:[],
+      createPostVisible: false,
+      postLoading: false,
+      selectedSort: '',
+      sortOptions:[
+        {value: 'title', name: 'По названию'},
+        {value: 'body', name: 'По содержимому'}
+      ]
     }
   },
   methods: {
@@ -52,8 +63,25 @@ export default {
     },
     openCreatePost(){
       this.createPostVisible = true
+    },
+    async fetchPost(){
+      try {
+        this.postLoading = true
+        setTimeout( async () => {
+          const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10')
+          this.posts = response.data  
+          this.postLoading = false        
+        }, 1000)
+      } catch (error) {
+        console.log(error)
+      }
     }
+  },
+
+  mounted(){
+    this.fetchPost()
   }
+
 }
 </script>
 
@@ -62,5 +90,14 @@ export default {
     display: flex;
     justify-content: center;
     align-items: center;
+  }
+  .loading{
+    margin: 15px;
+    font-size: 20px;
+    font-family:Verdana, Geneva, Tahoma, sans-serif
+  }
+  .app__btns{
+    display: flex;
+    justify-content: space-between;
   }
 </style>
--- src/components/myUI/index.js
+++ src/components/myUI/index.js
@@ -1,6 +1,7 @@
 import MyBtn from '@/components/myUI/MyBtn'
 import MyInput from '@/components/myUI/MyInput'
 import MyCreatePost from '@/components/myUI/MyCreatePost'
+import MySelect from '@/components/myUI/MySelect'
 export default[
-    MyBtn, MyInput, MyCreatePost
+    MyBtn, MyInput, MyCreatePost, MySelect
 ]
